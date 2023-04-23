ef9d6276637ab591670c3f10c1562490d46b8c0e
Mikel <mikel.pannell@protonmail.com>
2023-04-23 15:01:35 -0600

    Add proper responsive navbar.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,17 +7,56 @@
 </svelte:head>
 
 <div class="navbar bg-base-200">
-    <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        </button>
+    <div class="dropdown lg:hidden">
+        <label tabindex="0" class="btn btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52">
+            <li class="disabled bg-base-300"><span>Home</span></li>
+            <li><a>About Us</a></li>
+            <li><a>Projects</a></li>
+            <li><a>Events</a></li>
+            <li>
+            <span class="justify-between">
+                Sponsors
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+            </span>
+                <ul class="p-2 bg-base-300">
+                    <li><a>Potential Sponsors</a></li>
+                    <li><a>Current Sponsors</a></li>
+                    <li><a>Why Sponsor Us</a></li>
+                </ul>
+            </li>
+            <li><a>Blog</a></li>
+            <li><a>Shop</a></li>
+            <li><a>Connect</a></li>
+        </ul>
     </div>
-    <div class="flex-1"></div>
-    <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-        </button>
+
+    <div class="flex-1">
+        <a href="/" class="btn btn-ghost normal-case text-xl">Hangar 84</a>
     </div>
+
+    <ul class="menu menu-horizontal gap-x-2 hidden lg:flex flex-none">
+        <li class="disabled bg-base-300"><span>Home</span></li>
+        <li><a>About Us</a></li>
+        <li><a>Projects</a></li>
+        <li><a>Events</a></li>
+        <li>
+            <span class="justify-between">
+                Sponsors
+                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+            </span>
+            <ul class="p-2 bg-base-300">
+                <li><a>Potential Sponsors</a></li>
+                <li><a>Current Sponsors</a></li>
+                <li><a>Why Sponsor Us</a></li>
+           </ul>
+        </li>
+        <li><a>Blog</a></li>
+        <li><a>Shop</a></li>
+        <li><a>Connect</a></li>
+    </ul>
 </div>
 
 <div class="flex flex-col items-center py-16">
