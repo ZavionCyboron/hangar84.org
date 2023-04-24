8fc42740307d4a3e888ae03cbfa86ce8a7ed4484
Mikel <mikel.pannell@protonmail.com>
2023-04-23 20:42:53 -0600

    Revert "Add meta to error pages."
    This reverts commit 4ace0e9cfecffa9351264f2dd21b7e6531ac2a3e.
    
    	modified:   src/routes/+error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,18 +2,6 @@
     import { page } from '$app/stores';
 </script>
 
-<svelte:head>
-    <title>{$page.status} {$page.error.message} | Hangar 84</title>
-    <meta content="{$page.status} {$page.error.message} | Hangar 84" property="og:title" />
-    {#if $page.status === 404}
-        <meta content="You shared a non-existant page. How awkward..." property="og:description" />
-    {:else}
-        <meta content="You shared a broken page. How very awkward..." property="og:description" />
-    {/if}
-    <meta content="https://hangar84.org/" property="og:url" />
-    <meta content="#ff0000" data-react-helmet="true" name="theme-color" />
-</svelte:head>
-
 <div class="text-center py-8">
     <h1 class="text-6xl font-bold">{$page.status} {$page.error.message}</h1>
     <br />
