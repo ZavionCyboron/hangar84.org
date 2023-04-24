26cada833adbf3354063a580c0c4cf31031d9989
Mikel <mikel.pannell@protonmail.com>
2023-04-23 20:03:07 -0600

    Slightly spice up error page.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<div class="text-center py-8">
+    <h1 class="text-6xl font-bold">{$page.status} {$page.error.message}</h1>
+    <br />
+    <h3 class="text-2xl">4xx = You broke it</h3>
+    <h3 class="text-2xl">5xx = I broke it</h3>
+</div>
