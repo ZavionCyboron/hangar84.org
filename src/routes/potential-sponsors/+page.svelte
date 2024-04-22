282d36772519deef1b7e27123e55d482c164680d
ZavionCyboron <z.cyboron07@gmail.com>
2024-04-22 16:13:48 -0600

    feat: add potential-sponsors page to the website

---
+++ src/routes/potential-sponsors/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Potential-sponsors | Hangar 84</title>
+	<meta content="Potential-sponsors | Hangar 84" property="og:title"/>
+	<meta content="Potential-sponsors for Hangar 84, FRC Team 7271" property="og:description"/>
+	<meta content="https://hangar84.org/potential-sponsors" property="og:url"/>
+	<meta content="#32cd32" data-react-helmet="true" name="theme-color"/>
+</svelte:head>
+
+<div class="flex flex-col items-center mt-8 mb-16 scroll-m-0">
+	<div class="shrink text-center flex flex-col space-y-6">
+		<h1 class="pb-3 text-6xl font-bold bg-gradient-to-r from-green-400 via-green-500 to-green-400 inline-block text-transparent bg-clip-text">
+			Potential sponsors
+		</h1>
+		<h3 class="text-2xl text-center"> Coming soon</h3>
+	</div>
+</div>
