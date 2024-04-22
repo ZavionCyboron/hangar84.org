e089f81ebca64337fe2ce8b41f9db93d683108fe
ZavionCyboron <z.cyboron07@gmail.com>
2024-04-22 16:11:22 -0600

    feat: add about page to the website

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>About | Hangar 84</title>
+	<meta content="About | Hangar 84" property="og:title"/>
+	<meta content="Information about Hangar 84, FRC Team 7271" property="og:description"/>
+	<meta content="https://hangar84.org/about" property="og:url"/>
+	<meta content="#32cd32" data-react-helmet="true" name="theme-color"/>
+</svelte:head>
+
+<div class="flex flex-col items-center mt-8 mb-16">
+		<div class="shrink text-center flex flex-col space-y-6">
+			<h1 class="pb-3 text-6xl font-bold bg-gradient-to-r from-green-400 via-green-500 to-green-400 inline-block text-transparent bg-clip-text">
+				About Us
+			</h1>
+			<h3 class="text-xl"> Coming soon</h3>
+		</div>
+</div>
