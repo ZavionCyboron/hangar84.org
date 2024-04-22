24261d4576e264f496b77a2a14d18b9381cc6fa4
ZavionCyboron <z.cyboron07@gmail.com>
2024-04-22 16:12:57 -0600

    feat: add current-sponsors page to the website

---
+++ src/routes/current-sponsors/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Current-sponsors | Hangar 84</title>
+	<meta content="Current-sponsors | Hangar 84" property="og:title"/>
+	<meta content="People who are currently sponsoring Hangar 84, FRC Team 7271" property="og:description"/>
+	<meta content="https://hangar84.org/current-sponsors" property="og:url"/>
+	<meta content="#32cd32" data-react-helmet="true" name="theme-color"/>
+</svelte:head>
+
+<div class="flex flex-col items-center mt-8 mb-16 scroll-m-0">
+	<div class="shrink text-center flex flex-col space-y-6">
+		<h1 class="pb-3 text-6xl font-bold bg-gradient-to-r from-green-400 via-green-500 to-green-400 inline-block text-transparent bg-clip-text">
+			Current sponsors
+		</h1>
+		<h3 class="text-2xl text-center"> Coming soon</h3>
+	</div>
+</div>
