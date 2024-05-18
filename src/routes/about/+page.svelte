1fb80b7360932da824755fcc58fd8de6c5728ba1
ZavionCyboron <z.cyboron07@gmail.com>
2024-05-18 14:03:12 -0600

    feat: added a descriptive paragraph to the about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,6 +4,11 @@
     <meta content="Information about Hangar 84 and our goals." property="og:description"/>
     <meta content="https://hangar84.org/about" property="og:url"/>
     <meta content="#32cd32" data-react-helmet="true" name="theme-color"/>
+    <style>
+        p {
+
+        }
+    </style>
 </svelte:head>
 
 <div class="flex flex-col items-center mt-8 mb-16">
@@ -11,6 +16,10 @@
         <h1 class="pb-3 text-6xl font-bold bg-gradient-to-r from-green-400 via-green-500 to-green-400 inline-block text-transparent bg-clip-text">
             About Us
         </h1>
-        <h2 class="text-2xl">Coming soon</h2>
+        <p class="pb-3 text-xl">
+            Hangar84 is a student led robotics competition team. We have gone to five competitions but our goal during
+            these competitions is not win them but to allow the students to have fun and also learn and improve on the
+            robot.
+        </p>
     </div>
 </div>
