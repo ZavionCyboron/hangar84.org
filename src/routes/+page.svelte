83471b5e93e436f4b242e81eb1b92f2ad5f19b5a
ZavionCyboron <z.cyboron07@gmail.com>
2024-05-18 13:27:38 -0600

    feat: added FRC competition pictures to the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
     <meta content="Official website for Hangar 84, FRC Team 7271" property="og:description"/>
     <meta content="https://hangar84.org/" property="og:url"/>
     <meta content="#32cd32" data-react-helmet="true" name="theme-color"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./output.css" rel="stylesheet"/>
 </svelte:head>
 
 <div class="flex flex-col items-center mt-8 mb-16">
@@ -12,7 +14,39 @@
             Hangar 84
         </h1>
     </div>
-    <div>
-        <img src="favicon.png" alt="Favicon hasn't appeared."/>
+    <div class="h-max w-72 carousel rounded-box">
+        <div class="carousel-item w-full">
+            <img src="frc-competition-1.jpg" alt="FRC competition didn't load." />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="frc-competition-2.jpg" alt="FRC competition didn't load." />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="frc-competition-3.jpg" alt="FRC competition didn't load." />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="frc-competition-4.jpg" alt="FRC competition didn't load." />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="frc-competition-5.jpg" alt="FRC competition didn't load." />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="frc-competition-6.jpg" alt="FRC competition didn't load." />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="frc-competition-7.jpg" alt="FRC competition didn't load." />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="frc-competition-9.jpg" alt="FRC competition didn't load." />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="frc-competition-10.jpg" alt="FRC competition didn't load." />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="frc-competition-11.jpg" alt="FRC competition didn't load." />
+        </div>
+        <div class="carousel-item w-full">
+            <img src="frc-competition-12.jpg" alt="FRC competition didn't load." />
+        </div>
     </div>
 </div>
