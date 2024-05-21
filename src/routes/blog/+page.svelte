80f2f1cc511548fdd4c48aad22c0c83518181b1e
ZavionCyboron <z.cyboron07@gmail.com>
2024-05-21 12:31:52 -0600

    feat: add Youtube video

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -12,7 +12,7 @@
             Blog
         </h1>
         <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/Vo9HBRpWLyA?si=i9LCvoaHGCOwnAru" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Vo9HBRpWLyA?si=i9LCvoaHGCOwnAru" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     </div>
 </div>
