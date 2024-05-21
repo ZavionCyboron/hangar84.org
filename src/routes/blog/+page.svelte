737429a6390421ed1d04cdde934fd895da474512
ZavionCyboron <z.cyboron07@gmail.com>
2024-05-21 12:29:37 -0600

    feat: add Youtube video

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -11,6 +11,8 @@
         <h1 class="pb-3 text-6xl font-bold bg-gradient-to-r from-green-400 via-green-500 to-green-400 inline-block text-transparent bg-clip-text">
             Blog
         </h1>
-        <h2 class="text-2xl">Coming soon</h2>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Vo9HBRpWLyA?si=i9LCvoaHGCOwnAru" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
     </div>
 </div>
