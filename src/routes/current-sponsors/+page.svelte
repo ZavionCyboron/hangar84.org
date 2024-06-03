aad39e2d1c9caef04fa5e6f81ebab96fd60954ae
ZavionCyboron <z.cyboron07@gmail.com>
2024-06-03 12:26:54 -0600

    feat: add current sponsors and a clickable xlsx sheet

--- src/routes/current-sponsors/+page.svelte
+++ src/routes/current-sponsors/+page.svelte
@@ -1,3 +1,26 @@
+<script>
+    import { onMount } from 'svelte';
+    import { read, utils, writeFileXLSX } from 'xlsx';
+
+    /* the component state is an array of presidents */
+    let sponsors = [    { ActiveSponsors: "Pecos Valley Telecomunications"}];
+
+    /* Fetch and update the state once */
+    onMount(async() => {
+        const f = await (await fetch("https://hangar-84.monday.com/boards/4167895144.xlsx")).arrayBuffer();
+        const wb = read(f); // parse the array buffer
+        const ws = wb.Sheets[wb.SheetNames[0]]; // get the first worksheet
+        sponsors = utils.sheet_to_json(ws); // generate objects and update state
+    });
+
+    /* get state data and export to XLSX */
+    function exportFile() {
+        const ws = utils.json_to_sheet(sponsors);
+        const wb = utils.book_new();
+        utils.book_append_sheet(wb, ws, "Data");
+        writeFileXLSX(wb, "Sponsors.xlsx");
+    }
+</script>
 <svelte:head>
     <title>Current Sponsors | Hangar 84</title>
     <meta content="Current Sponsors | Hangar 84" property="og:title"/>
@@ -11,6 +34,18 @@
         <h1 class="pb-3 text-6xl font-bold bg-gradient-to-r from-green-400 via-green-500 to-green-400 inline-block text-transparent bg-clip-text">
             Current Sponsors
         </h1>
-        <h2 class="text-2xl">Coming soon</h2>
+        <div class="flex flex-col items-center mt-4 mb-8">
+            <table><thead><tr><th class="text-xl">Active Sponsors</th></tr></thead><tbody>
+            {#each sponsors as p}<tr>
+                <td class="text-2xl font-bold bg-gradient-to-r from-green-600 via-green-600 to-green-600 inline-block text-transparent bg-clip-text">
+                    {p.ActiveSponsors}
+                </td>
+            </tr>{/each}
+            </tbody><thead><td>
+                <button on:click={exportFile} class="text-2xl"><u class="pb-3 text-2xl font-bold bg-gradient-to-r from-blue-400 via-blue-400 to-blue-400 inline-block text-transparent bg-clip-text">
+                    Sponsors.xlsx
+                </u></button>
+            </td></thead></table>
+        </div>
     </div>
 </div>
