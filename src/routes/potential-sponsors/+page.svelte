ff46f1739c276ef1c455718fda3f6c7e5dd9bbc1
ZavionCyboron <z.cyboron07@gmail.com>
2024-06-09 14:56:37 -0600

    feat: add form for potential sponsors

--- src/routes/potential-sponsors/+page.svelte
+++ src/routes/potential-sponsors/+page.svelte
@@ -11,9 +11,30 @@
 		<h1 class="pb-3 text-6xl font-bold bg-gradient-to-r from-green-400 via-green-500 to-green-400 inline-block text-transparent bg-clip-text">
 			Potential Sponsors
 		</h1>
-		<div class="text-2xl flex flex-col items-center mt-4 mb-8">
-			<a href="https://hangar-84.monday.com/boards/4167895144"><u class="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-400 text-transparent bg-clip-text">
-				Potential Sponsors</u></a>
-		</div>
 	</div>
+</div>
+<div>
+	<form method="post" action="mailto:z.cyboron07@gmail.com">
+		<label>
+			Sponsor name:
+			<input type="text" name="sponsors" size="40" class="text-center"> <br>
+		</label>
+		<label>
+			<!-- put a name for the input text of amount of money they want to give -->
+			<input type="text" name="Sponsorship money" class="text-center"> <br>
+		</label>
+		<label>
+			Email:
+			<input type="email" name="company email" placeholder="you@yourdomanin.com" class="text-center"> <br>
+		</label>
+		<label>
+			Phone Number:
+			<input type="tel" name="mobile" placeholder="(xxx)-xxx-xxxx" class="text-center"> <br>
+		</label>
+		<label>
+			Date of submission
+			<input type="date" name="date" placeholder="06/09/2024" class="text-center"> <br>
+		</label>
+		<button type="submit" value="Form submitted" style="border-radius: 15px; box-shadow: 5px 5px 5px green;">Submit</button>
+	</form>
 </div>
