262bfaf0c5b39688f6a028f898a6a1d3689440af
Zavion Cyboron <z.cyboron07@gmail.com>
2024-06-28 13:43:47 -0600

    feat: add links to pages potential-sponsors and current-sponsors

--- src/routes/why-sponsor-us/+page.svelte
+++ src/routes/why-sponsor-us/+page.svelte
@@ -11,10 +11,12 @@
         <h1 class="pb-3 text-6xl font-bold bg-gradient-to-r from-green-400 via-green-500 to-green-400 inline-block text-transparent bg-clip-text">
             Why Sponsor Us?
         </h1>
-        <div style="background-image: url('favicon.png');">
+        <div>
             <p class="text-xl">
                 <!--put our reason for wanting other companies/organizations to sponsor us. Also give the company/organization benefits of being sponsored.-->
-            </p>
+            </p><br>
+            <a href="https://hangar84.org/potential-sponsors/" style="border: 1px solid green; border-radius: 15px" class="text-2xl">Potential Sponsors</a><br><br>
+            <a href="https://hangar84.org/current-sponsors/" style="border: 1px solid green; border-radius: 15px;" class="text-2xl">Current Sponsors</a><br>
         </div>
     </div>
 </div>
