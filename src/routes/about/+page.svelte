91bf86e0e4610e4e580018c722c4739c211aab4d
Zavion Cyboron <z.cyboron07@gmail.com>
2024-06-29 20:43:49 -0600

    refactor: change paragraph on the about us page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,15 +6,18 @@
     <meta content="#32cd32" data-react-helmet="true" name="theme-color"/>
 </svelte:head>
 
-<div class="flex flex-col items-center mt-8 mb-16">
+<div class="flex flex-col items-center text-center mt-8 mb-16">
     <div class="shrink text-center flex flex-col space-y-4">
         <h1 class="pb-3 text-6xl font-bold bg-gradient-to-r from-green-400 via-green-500 to-green-400 inline-block text-transparent bg-clip-text">
             About Us
         </h1>
+        <!-- Paragraph that tells viewers about us -->
         <p class="pb-3 text-xl">
-            Hangar 84 is a student-led robotics competition team. We have gone to five competitions but our goal during
-            these competitions is not win them but to allow the students to have fun and also learn and improve on the
-            robot.
-        </p>
+            We are a student-led FRC robotics team at Early College High School in Roswell, New Mexico.
+            The year we started as a FRC robotics team was 2018 and have been to every competition besides 2021 and 2022 competitions.
+            At Hangar 84, we are open to any suggestions on how to improve not only our robot designs but also our source code and website.
+            We also accept anyone that wants to join our team. If you want to join our team give us an email at (insert email here).
+        </p><br>
+        <!-- put anything extra here such as images, videos, or links -->
     </div>
 </div>
