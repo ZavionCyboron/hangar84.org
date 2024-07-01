d04261cd74f2c372a80a773d39fe17c7e8338b40
Zavion Cyboron <z.cyboron07@gmail.com>
2024-06-30 18:41:57 -0600

    feat: add paragraph to why-sponsor-us page

--- src/routes/why-sponsor-us/+page.svelte
+++ src/routes/why-sponsor-us/+page.svelte
@@ -13,7 +13,11 @@
         </h1>
         <div>
             <p class="text-xl">
-                <!--put our reason for wanting other companies/organizations to sponsor us. Also give the company/organization benefits of being sponsored.-->
+                <!-- This is a draft and will be improved or changed later.-->
+                Why sponsor us? The reason why we would like you to sponsor us is to allow us to be able to go on trips for FRC competitions,
+                buy better parts and pieces for our robot to allow us to have a better chance in winning a competition.
+                If you sponsor us we will put your company, organization, etc. logo on the back of our shirt.
+                We will also give you (insert items we give here. such as shirt, logo, and more.)
             </p><br>
             <a href="https://hangar84.org/potential-sponsors/" style="box-shadow: 5px 5px 5px blue; border-radius: 15px" class="text-2xl">Potential Sponsors</a><br><br>
             <a href="https://hangar84.org/current-sponsors/" style="box-shadow: 5px 5px 5px blue; border-radius: 15px;" class="text-2xl">Current Sponsors</a><br>
