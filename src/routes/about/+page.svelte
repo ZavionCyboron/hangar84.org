0b30bf9efe4ffe05c6ac8cd8b644d8f97d0b2008
Zavion Cyboron <z.cyboron07@gmail.com>
2024-07-30 08:26:30 -0600

    feat: add Gmail link to the about us paragraph

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,13 +11,15 @@
         <h1 class="pb-3 text-6xl font-bold bg-gradient-to-r from-green-400 via-green-500 to-green-400 inline-block text-transparent bg-clip-text">
             About Us
         </h1>
-        <!-- Paragraph that tells viewers about us -->
+        <!--Paragraph that tells viewers about us-->
         <p class="pb-3 text-xl">
             We are a student-led FRC robotics team at Early College High School in Roswell, New Mexico.
             We started our group in 2018 and we have competed in every FRC competition besides 2021 and 2022 competitions.
             At Hangar 84, we are open to any suggestions on how to improve not only our robot designs, but also our source code and website.
-            We also accept anyone that wants to join our team. If you want to join our team send us an email at (insert email here).
+            We also accept anyone that wants to join our team. If you want to join our team send us an email at
+            <a href="https://mail.google.com/mail/u/1/#inbox?compose=GTvVlcSKhbwcTxZpkSmmnwVrZLtjzjKZqkDjCCMprXSMLnqcWTWFSTCxZffLGwXZnhhrLwbNsSVCT">
+                <p class="font-bold bg-gradient-to-r from-blue-400 via-blue-400 to-blue-400 inline-block text-transparent bg-clip-text">hanger84robotics@gmail.com</p></a>
         </p><br>
-        <!-- put anything extra here such as images, videos, or links -->
+        <!--put anything extra here such as images, videos, or links-->
     </div>
 </div>
