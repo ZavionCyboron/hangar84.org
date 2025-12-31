ec491bd83bdce66f5b3ef39be880cb33389c169c
Zavion Cyboron <z.cyboron07@gmail.com>
2025-12-31 01:05:20 -0700

    fix: merge from develop into main

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,7 +6,7 @@
     <title>{$page.status} {$page.error.message} | Hangar 84</title>
     <meta content="{$page.status} {$page.error.message} | Hangar 84" property="og:title" />
     {#if $page.status === 404}
-        <meta content="You shared a non-existant page. How awkward..." property="og:description" />
+        <meta content="You shared a non-existent page. How awkward..." property="og:description" />
     {:else}
         <meta content="You shared a broken page. How very awkward..." property="og:description" />
     {/if}
@@ -14,8 +14,8 @@
     <meta content="#ff0000" data-react-helmet="true" name="theme-color" />
 </svelte:head>
 
-<div class="flex flex-col text-center justify-center items-center py-8 space-y-4">
-    <h1 class="text-6xl font-bold">{$page.status} — {$page.error.message}</h1>
+<div class="text-center py-8">
+    <h1 class="text-6xl font-bold">{$page.status} {$page.error.message}</h1>
     <br />
     {#if $page.status === 404}
         <div>
